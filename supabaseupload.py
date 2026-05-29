34faca9f23135f1f4249a8ba94b270847832945c
Hackprakas <vijayatrprakash@gmail.com>
2026-05-29 14:31:04 +0530

    last commit

--- supabaseupload.py
+++ supabaseupload.py
@@ -64,6 +64,8 @@ async def get_user_from_token(token: str = Depends(oauth2_scheme)):
 async def check_user_permissio(user: dict):
     if not user.get("emailss"):
         return {"error": "No user found."}
+    else:
+        print("nothing")
 
     
     permissions = user.get("permissions", {})
